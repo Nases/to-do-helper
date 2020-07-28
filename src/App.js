0e9c38af4935d690c6158d981bdfae5eb1e67afe
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-07-27 18:47:02 -0700

    '🔹'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import '../node_modules/react-grid-layout/css/styles.css'
+import '../node_modules/react-resizable/css/styles.css'
+
+import Test from './components/Test'
 
 function App() {
   return (
     <div className="App">
-
-
+      <Test />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
